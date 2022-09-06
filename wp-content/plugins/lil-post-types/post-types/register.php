7a69f56643e8b992df6a446070d43faa0c0a3a54
Crystal McNeil <crystalsmcneil@gmail.com>
2022-09-06 17:11:02 -0600

    updates register args

--- wp-content/plugins/lil-post-types/post-types/register.php
+++ wp-content/plugins/lil-post-types/post-types/register.php
@@ -6,9 +6,9 @@ function lil_register_business_type()
 		'name' => __('Businesses', LILDOMAIN),
 		'singular_name' => __('Business', LILDOMAIN),
 		'featured_image' => __('Business Logo', LILDOMAIN),
-		'set_feature_image' => __('Set Business Logo', LILDOMAIN),
-		'remove_feature_image' => __('Remove Logo', LILDOMAIN),
-		'use_feature_image' => __('Use Logo', LILDOMAIN),
+		'set_featured_image' => __('Set Business Logo', LILDOMAIN),
+		'remove_featured_image' => __('Remove Logo', LILDOMAIN),
+		'use_featured_image' => __('Use Logo', LILDOMAIN),
 		'archives' => __('Business Directory', LILDOMAIN),
 		'add_new' => __('Add New Business', LILDOMAIN),
 		'add_new_item' => __('Add New Business', LILDOMAIN),
@@ -17,6 +17,11 @@ function lil_register_business_type()
 	$args = array(
 		'labels' => $labels,
 		'public' => true,
+		'has_archive' => 'businesses',
+		'rewrite' => array('has_front' => true),
+		'menu_icon' => 'dashicons-building',
+		'supports' => array('title', 'editor', 'thumbnail'),
+		'show_in_rest' => true,
 	);
 
 	register_post_type('business', $args);
