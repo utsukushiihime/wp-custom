13d9d673bd7a1c6066477de930cef6fd474a81a1
Crystal McNeil <crystalsmcneil@gmail.com>
2022-09-06 17:23:27 -0600

    adds events skeleton

---
+++ wp-content/plugins/events/event-posts.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Events
+ * Plugin URI:  http://github.com/jcasabona/lil-post-types
+ * Description: A simple plugin for creating events.
+ * Version: 1.0.0
+ * Author: Crystal McNeil
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/license/gpl-2.0.text
+ * Text Domain: event-posts
+ * Domain Path: /languages
+ */
